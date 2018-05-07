4f05ff9f5bc96da5c2c4d3c40fa429b52cc0cfec
Ben Greenman <benjaminlgreenman@gmail.com>
2018-05-07 19:16:21 -0400

    ** add package dependencies

--- info.rkt
+++ info.rkt
@@ -1,10 +1,11 @@
 #lang info
 (define collection "gtp-benchmarks")
-(define deps '("base" "typed-racket-lib" "typed-racket-more"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "typed-racket-doc"))
+(define deps '("base" "typed-racket-lib" "typed-racket-more" "require-typed-check"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "typed-racket-doc" "at-exp-lib" "gtp-util" "pict-lib" "scribble-abbrevs" "syntax-sloc" "with-cache"))
 (define pkg-desc "Gradual typing benchmark programs")
-(define version "0.1")
+(define version "0.2")
 (define pkg-authors '(ben))
 (define scribblings '(("scribblings/gtp-benchmarks.scrbl" () ("Benchmarks"))))
 (define compile-omit-paths '("benchmarks"))
 (define test-omit-paths '("benchmarks"))
+
