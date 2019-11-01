fdaf3f52e4accdef149c9858e3f05eebd00179bd
Cameron Moy <camoy@cs.umd.edu>
2019-11-01 12:19:40 -0400

    zombie: Update untyped to use same assertion

--- benchmarks/zombie/untyped/main.rkt
+++ benchmarks/zombie/untyped/main.rkt
@@ -24,12 +24,21 @@
      [(to-draw stop-when)
        (loop w (cdr h))]
      [(on-mouse)
-      (define r (apply (world-on-mouse w) (if (and (list? as) (real? (car as)) (real? (cadr as)) (string? (caddr as)) (null? (cdddr as)))
+      (define r (apply (world-on-mouse w) (if (real-real-string-list? as)
                                               as (error "cast error"))))
       (loop r (cdr h))]
      [(on-tick)
       (define r ((world-on-tick w)))
       (loop r (cdr h))])])))
+
+(define (real-real-string-list? x)
+  (and (pair? x)
+       (pair? (cdr x))
+       (pair? (cdr (cdr x)))
+       (null? (cdr (cdr (cdr x))))
+       (real? (car x))
+       (real? (car (cdr x)))
+       (string? (car (cdr (cdr x))))))
 
 (define DATA
   (with-input-from-file "../base/zombie-hist.rktd" read))
