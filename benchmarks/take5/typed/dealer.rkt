b5d6ecfca65e8506f91e77574fff05d983c8bdc2
Lukas Lazarek <LukasALazarek@gmail.com>
2022-11-22 17:02:34 -0600

    take5: use players supplied to the dealer instead of making new ones

--- benchmarks/take5/typed/dealer.rkt
+++ benchmarks/take5/typed/dealer.rkt
@@ -66,10 +66,20 @@
 
     (field
      [internal% : Internal%
-      (class player%
+      (class object%
+        (super-new)
         (init-field player)
-        (super-new [n (send player name)] [order default-order])
-        (field [my-bulls 0])
+        (field [my-bulls 0]
+
+               ;; dummy fields, never used, only here to satisfy the interface
+               [n 0]
+               [order default-order]
+               [my-cards empty])
+        (define/public (name) (send player name))
+        (define/public (start-round cs) (send player start-round cs))
+        (define/public (start-turn d) (send player start-turn d))
+        (define/public (choose d) (send player choose d))
+
         (define/public (bulls) (get-field my-bulls this))
         (define/public (add-score n)
           (set-field! my-bulls this (+ n (get-field my-bulls this)))))]
