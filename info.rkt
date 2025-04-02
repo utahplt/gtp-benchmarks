f974dcba643880399df9fe643009e9f04dc58f19
Ben Greenman <benjaminlgreenman@gmail.com>
2025-04-02 16:14:18 -0400

    info.rkt: fix undeclared build dep

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "gtp-benchmarks")
 (define deps '("base" "typed-racket-lib" "typed-racket-more" "require-typed-check"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "typed-racket-doc" "at-exp-lib" "gtp-util" "pict-lib" "scribble-abbrevs" "syntax-sloc" "with-cache"))
+(define build-deps '("gtp-measure" "scribble-lib" "racket-doc" "rackunit-lib" "typed-racket-doc" "at-exp-lib" "gtp-util" "pict-lib" "scribble-abbrevs" "syntax-sloc" "with-cache"))
 (define pkg-desc "Gradual typing benchmark programs")
 (define version "9.3")
 (define pkg-authors '(ben))
